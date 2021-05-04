4f55aa3828d7f26b2e8dd4e0a674cb0c33304a97
kenneth-fernandes <kenneth.peter.fernandes@gmail.com>
2021-05-04 03:48:25 -0400

    Kenneth: Added useUnifiedTopology option to use new Server Discover and Monitoring engine of mongoDB.

--- config/db.js
+++ config/db.js
@@ -5,7 +5,10 @@ const db = config.get('mongoURI');
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true });
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log('MongoDB connected....');
   } catch (error) {
