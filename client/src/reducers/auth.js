f75c30eee6d515acf1bd91735b24ec66e944911d
kenneth-fernandes <kenneth.peter.fernandes@gmail.com>
2021-05-08 23:15:58 -0400

    Kenneth: Fixed error

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -26,7 +26,8 @@ export default (state = initialState, action) => {
     case REGISTER_SUCCESS:
       localStorage.setItem('token', payload.token);
       return { ...state, ...payload, isAuthenticated: true, loading: false };
-    case (AUTH_ERROR, REGISTER_FAIL):
+    case AUTH_ERROR:
+    case REGISTER_FAIL:
       localStorage.removeItem('token');
       return {
         ...state,
