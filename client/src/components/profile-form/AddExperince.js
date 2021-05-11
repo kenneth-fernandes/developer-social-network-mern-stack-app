0eccd8762ff23419bfa584aabab9f2c073b332cd
kenneth-fernandes <kenneth.peter.fernandes@gmail.com>
2021-05-11 14:47:20 -0400

    Kenneth: Updated links.

--- client/src/components/profile-form/AddExperince.js
+++ client/src/components/profile-form/AddExperince.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addExperience } from '../../actions/profile';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 const AddExperince = ({ addExperience, history }) => {
   const [formData, setFormData] = useState({
@@ -111,9 +111,9 @@ const AddExperince = ({ addExperience, history }) => {
           ></textarea>
         </div>
         <input type='submit' className='btn btn-primary my-1' />
-        <a className='btn my-1' href='dashboard.html'>
+        <Link className='btn my-1' to='/dashboard'>
           Go Back
-        </a>
+        </Link>
       </form>
     </Fragment>
   );
