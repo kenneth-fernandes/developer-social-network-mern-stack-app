4c865abe2b0228c41ea8cf1b8e9e6cceb7e2f97e
kenneth-fernandes <kenneth.peter.fernandes@gmail.com>
2021-05-11 03:01:42 -0400

    Kenneth: Added deployment code.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { usersRouter } from './routes/api/users.js';
 import { postsRouter } from './routes/api/posts.js';
 import { profileRouter } from './routes/api/profile.js';
 import { authRouter } from './routes/api/auth.js';
+import path from 'path';
 
 const app = express();
 
@@ -13,16 +14,20 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('API running');
-});
-
 // Define routes
 app.use('/api/users', usersRouter);
 app.use('/api/posts', postsRouter);
 app.use('/api/profile', profileRouter);
 app.use('/api/auth', authRouter);
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index,html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
