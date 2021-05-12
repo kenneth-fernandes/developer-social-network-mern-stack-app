677e650d2cdb2bba03b0af210922862ee6530c19
kenneth-fernandes <kenneth.peter.fernandes@gmail.com>
2021-05-12 01:28:32 -0400

    Kenneth: Applied fix.

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ import { postsRouter } from './routes/api/posts.js';
 import { profileRouter } from './routes/api/profile.js';
 import { authRouter } from './routes/api/auth.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -24,10 +27,13 @@ app.use('/api/auth', authRouter);
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
+    console.log(__dirname);
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
